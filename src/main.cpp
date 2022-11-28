022e3d8a851c9205d540cbd0912cc9d1ac903b5e
Julien Boriasse <julien@boriasse.com>
2022-11-28 17:19:03 -0500

    Adjust pwm calculation to pwm resolution

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,10 @@
 #include "WiFi.h"
 #include <PubSubClient.h>
 
+#define NUMBER_OF_MOTORS 6
+#define MOTOR_PWM_DUTY_CYCLE_MINIMUM 3.5
+#define MOTOR_PWM_DUTY_CYCLE_MAXIMUM 11.5
+
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
 
@@ -11,9 +15,14 @@ const char * MQTTServer = "broker.hivemq.com";
 WiFiClient wifiClient;
 PubSubClient MQTTClient(wifiClient);
 
-#define NUMBER_OF_MOTORS 6
-#define MOTOR_PWM_DUTY_CYCLE_MINIMUM 3.5
-#define MOTOR_PWM_DUTY_CYCLE_MAXIMUM 11.5
+int dutyCycle;
+
+const int LEDPin = BUILTIN_LED; /* GPIO16 */
+const int motorPin[NUMBER_OF_MOTORS] = {13, 12, 27, 33, 15, 32};
+
+/* Setting PWM Properties */
+const int PWMFreq = 50; /* 50 Hz or 20ms for HS-422 servo motors */
+const int PWMResolution = 10;
 
 /*
   Function to set motor PWM duty cycle in percent
@@ -25,8 +34,10 @@ void motorWrite(uint8_t chan, float duty)
   duty = duty < MOTOR_PWM_DUTY_CYCLE_MINIMUM ? MOTOR_PWM_DUTY_CYCLE_MINIMUM : duty;
   duty = duty > MOTOR_PWM_DUTY_CYCLE_MAXIMUM ? MOTOR_PWM_DUTY_CYCLE_MAXIMUM : duty;
   /* scale value from 0-100 to 0-255 */
-  duty = duty * 255 / 100;
-  ledcWrite(chan, (uint32_t)duty);
+  duty = duty * (2^PWMResolution / 100);
+  uint32_t duty_int = (uint32_t) duty;
+  printf("Set motor channel %u to %u\r\n", chan, duty_int);
+  ledcWrite(chan, duty_int);
 }
 
 void callback(char *topic, byte *payload, unsigned int length)
@@ -135,16 +146,7 @@ void connectToNetwork()
   Serial.println("Connected to network");
 }
 
-int dutyCycle;
 
-const int LEDPin = BUILTIN_LED; /* GPIO16 */
-const int motorPin[NUMBER_OF_MOTORS] = {13, 12, 27, 33, 15, 32};
-
-/* Setting PWM Properties */
-const int PWMFreq = 50; /* 50 Hz or 20ms for HS-422 servo motors */
-const int PWMResolution = 10;
-const int MIN_DUTY_CYCLE = 3;
-const int MAX_DUTY_CYCLE = 12;
 
 
 
@@ -192,18 +194,4 @@ void loop()
   }
 
 
-  /* Increasing the motors positions with PWM */
-  for (dutyCycle = 0; dutyCycle <= MAX_DUTY_CYCLE; dutyCycle++)
-  {
-    for (int PWMChannel = 0; PWMChannel < NUMBER_OF_MOTORS; PWMChannel++)
-      motorWrite(PWMChannel, dutyCycle);
-    delay(3);
-  }
-  /* Decreasing the motors positions with PWM */
-  for (dutyCycle = MAX_DUTY_CYCLE; dutyCycle >= 0; dutyCycle--)
-  {
-    for (int PWMChannel = 0; PWMChannel < NUMBER_OF_MOTORS; PWMChannel++)
-      motorWrite(PWMChannel, dutyCycle);
-    delay(3);
-  }
 }
