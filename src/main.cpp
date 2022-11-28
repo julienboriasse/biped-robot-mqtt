6913acbf69cde887bdd151326434e5c382ea0474
Julien Boriasse <julien@boriasse.com>
2022-11-28 13:28:39 -0500

    WiFi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,76 @@
 #include <Arduino.h>
+#include "secrets.h"
+#include "WiFi.h"
+
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
 
 #define NUMBER_OF_MOTORS 6
 #define MOTOR_PWM_DUTY_CYCLE_MINIMUM 3.5
 #define MOTOR_PWM_DUTY_CYCLE_MAXIMUM 11.5
+
+String translateEncryptionType(wifi_auth_mode_t encryptionType)
+{
+
+  switch (encryptionType)
+  {
+  case (WIFI_AUTH_OPEN):
+    return "Open";
+  case (WIFI_AUTH_WEP):
+    return "WEP";
+  case (WIFI_AUTH_WPA_PSK):
+    return "WPA_PSK";
+  case (WIFI_AUTH_WPA2_PSK):
+    return "WPA2_PSK";
+  case (WIFI_AUTH_WPA_WPA2_PSK):
+    return "WPA_WPA2_PSK";
+  case (WIFI_AUTH_WPA2_ENTERPRISE):
+    return "WPA2_ENTERPRISE";
+  default:  
+    return "UNKNOWN";
+  }
+  return "UNKNOWN";
+}
+
+void scanNetworks()
+{
+
+  int numberOfNetworks = WiFi.scanNetworks();
+
+  Serial.print("Number of networks found: ");
+  Serial.println(numberOfNetworks);
+
+  for (int i = 0; i < numberOfNetworks; i++)
+  {
+
+    Serial.print("Network name: ");
+    Serial.println(WiFi.SSID(i));
+
+    Serial.print("Signal strength: ");
+    Serial.println(WiFi.RSSI(i));
+
+    Serial.print("MAC address: ");
+    Serial.println(WiFi.BSSIDstr(i));
+
+    Serial.print("Encryption type: ");
+    String encryptionTypeDescription = translateEncryptionType(WiFi.encryptionType(i));
+    Serial.println(encryptionTypeDescription);
+    Serial.println("-----------------------");
+  }
+}
+
+void connectToNetwork()
+{
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    Serial.println("Establishing connection to WiFi..");
+  }
+
+  Serial.println("Connected to network");
+}
 
 int dutyCycle;
 
@@ -31,6 +99,14 @@ void motorWrite(uint8_t chan, float duty)
 
 void setup()
 {
+  Serial.begin(115200);
+
+  scanNetworks();
+  connectToNetwork();
+
+  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.localIP());
+  
   /* Initialize PWM channel and pin for each motor */
   for (int PWMChannel = 0; PWMChannel < NUMBER_OF_MOTORS; PWMChannel++)
   {
